9376f5903b7cda081ef0a118a6b56ecf18811cc2
Emanuele di Vizio <Emanuele@GB-LOC-AL-29376.local>
2017-08-16 15:09:45 +0100

    android fix

--- android/src/main/java/com/googlecast/GoogleCastPackage.java
+++ android/src/main/java/com/googlecast/GoogleCastPackage.java
@@ -26,7 +26,7 @@ public class GoogleCastPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
+
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
